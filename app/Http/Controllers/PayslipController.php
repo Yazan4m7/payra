ca9fc20cabff54ed34dec9cdc2c5a2d9cd5d56ea
Yazan Abulaila <34026407+Yazan4m7@users.noreply.github.com>
2026-09-05 00:59:46 +0300

    Add approved-payroll YTD totals and reconciliation

--- app/Http/Controllers/PayslipController.php
+++ app/Http/Controllers/PayslipController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Payslip;
+use App\Services\PayrollReportingService;
 use Barryvdh\DomPDF\Facade\Pdf;
 
 class PayslipController extends Controller
 {
-    public function show(Payslip $payslip)
+    public function show(Payslip $payslip, PayrollReportingService $reporting)
     {
         $user = request()->user();
         if ($user->role === 'employee') {
@@ -17,9 +18,10 @@ class PayslipController extends Controller
         }
 
         $payslip->load('employee', 'payrollRun', 'payrollRun.complianceSetting');
+        $ytd = $reporting->ytdForEmployee($payslip->employee, $payslip->payrollRun->year, $payslip->payrollRun->month);
         $filename = "payslip-{$payslip->employee_id}-{$payslip->payrollRun->year}-{$payslip->payrollRun->month}.pdf";
 
-        return Pdf::loadView('pdf.payslip', ['payslip' => $payslip])
+        return Pdf::loadView('pdf.payslip', ['payslip' => $payslip, 'ytd' => $ytd])
             ->setPaper('a4')
             ->download($filename);
     }
